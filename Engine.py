ef74de4e2e5302c2673fe66ad4ed62c78de58aeb
Arthur <arthur@onefootabroad.com>
2016-12-02 20:53:05 -0300

    [WikiCore] Working =D

--- Engine.py
+++ Engine.py
@@ -14,5 +14,6 @@ for page_index in range(40):
 	cand = core.generateCandidates(wikipediaPageList[page_index])
 	print "--"
 	for (f,w,i,j,ind) in cand:
+		#f  similaridade entre texto e titulo do link , w o termo sem stopword, i posição no array das palavras , j quantas palavras a partir do inicio, ind indice do link   
 		print str(f)+" || "+w+" || "+wikipediaPageList[ind].title+" | "+str(i)+" "+str(j)
 	print "------------------------------------------------------------"
