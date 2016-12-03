3eba2a38767c65c07a5c9216d6ef70558ea181f8
m4nuela <manu_ela10@hotmail.com>
2016-12-03 07:39:54 -0300

    Crawler OK, zip finalFiles and GraphDistance function

--- Engine.py
+++ Engine.py
@@ -16,30 +16,25 @@ wikipediaPageList = Util.wikipediaPageList
 
 core = WikiCore(wikipediaPageList)
 
-for page_index in range(40):
-	print wikipediaPageList[page_index].title
-	cand = core.generateCandidates(wikipediaPageList[page_index])
-	print "--"
-	for (f,w,i,j,ind) in cand:
-		#f  similaridade entre texto e titulo do link , w o termo sem stopword, i posição no array das palavras , j quantas palavras a partir do inicio, ind indice do link
-		print str(f)+" || "+w+" || "+wikipediaPageList[ind].title+" | "+str(i)+" "+str(j)
-	print "------------------------------------------------------------"
+# for page_index in range(40):
+# 	print wikipediaPageList[page_index].title
+# 	cand = core.generateCandidates(wikipediaPageList[page_index])
+# 	print "--"
+# 	for (f,w,i,j,ind) in cand:
+# 		#f  similaridade entre texto e titulo do link , w o termo sem stopword, i posição no array das palavras , j quantas palavras a partir do inicio, ind indice do link
+# 		print str(f)+" || "+w+" || "+wikipediaPageList[ind].title+" | "+str(i)+" "+str(j)
+# 	print "------------------------------------------------------------"
 
 
 
 # Teste Graph Distance Manuela
 # wikipediaPage = wikipediaPageList[0]
-# vizinho = Util.getWikiPageFromUrl(u"https://en.wikipedia.org/wiki/Social_network")
+# dictionary = Util.dictionary
+#
+# vizinho = dictionary.get(u"https://en.wikipedia.org/wiki/Discrete_mathematics")
 #
 # dist=-1
 # if vizinho is not None:
 #     dist = Util.getMinimumDistance(wikipediaPage, vizinho)
-# print dist
+# print "Distancia : ",dist
 
-
-# print wikipediaPage.url
-# print wikipediaPage.title
-# print wikipediaPage.html
-# print wikipediaPage.content
-# print wikipediaPage.categoriesd
-# print wikipediaPage.links
--- Util.py
+++ Util.py
@@ -8,8 +8,10 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 notFound = True
-i = 1
 limit = 10
+minimumDepthFound = 11
+parent = None
+parentLinks = None
 
 path = os.path.dirname(os.path.abspath(__file__)) + '/files/'
 
@@ -44,48 +46,58 @@ def getWikiPageFromUrl(url):
             return wikiPage
     return None
 
+def getWikiPagesFromUrlDictionary():
+    dictionary = dict()
+    for wikiPage in wikipediaPageList:
+        dictionary[wikiPage.url] = wikiPage
+    return dictionary
 
 def getMinimumDistance(wikiPage1, wikiPage2):
     global notFound
-    global i
+    global minimumDepthFound
 
     if wikiPage1.url == wikiPage2.url:
-        distance = 0
+        minimumDepthFound = 0
     else:
         notFound = True
-        i = 1
-        distance = calculateGraphDistance(wikiPage1, wikiPage2)
+        calculateGraphDistance(wikiPage1, wikiPage2,0)
         notFound = True
-        i = 1
-        distance2 = calculateGraphDistance(wikiPage2, wikiPage1)
-        if distance > distance2:
-            distance = distance2
+        calculateGraphDistance(wikiPage2, wikiPage1,0)
+        #just to check the method is ok...
+        print "Parent",parent
+        print parentLinks
+    return minimumDepthFound
 
-    return distance
 
-
-def calculateGraphDistance(wikiPage1, wikiPage2):
+def calculateGraphDistance(wikiPage1, wikiPage2, depth):
     global notFound
-    global i
+    global minimumDepthFound
+    global parent
+    global parentLinks
 
+    depth = depth + 1
     links = [j[1]for j in wikiPage1.links]
     url = wikiPage2.url
 
-    if notFound | i <= limit:
+    if notFound | depth <= limit:
         if url in links:
             notFound = False
+            if depth < minimumDepthFound:
+                minimumDepthFound = depth
+                parent = wikiPage1.url
+                parentLinks = wikiPage1.links
+            return depth
         else:
             linksToBeVisited = []
             for link in links:
                 if notFound:
-                    wikiPageNeighbor = getWikiPageFromUrl(link)
+                    wikiPageNeighbor = dictionary.get(link)
                     if wikiPageNeighbor is not None:
                         linksToBeVisited.append(wikiPageNeighbor)
-            i = i + 1
             for wiki in linksToBeVisited:
-                calculateGraphDistance(wiki, wikiPage2)
-
-    return i
+                calculateGraphDistance(wiki, wikiPage2, depth)
+    return depth
 
 
 wikipediaPageList = getWikipediaPages()
+dictionary = getWikiPagesFromUrlDictionary()
