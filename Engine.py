68a071557ecd0b00e86925423220635c865aed91
Laryssa Seabra <llaryssa@me.com>
2016-12-04 02:18:33 -0300

    juntando as partes

--- Engine.py
+++ Engine.py
@@ -7,38 +7,44 @@ from bs4 import BeautifulSoup
 import re
 from WikiCore import WikiCore
 from Evaluation import evalCandidate
-
+from Evaluation import groupCandidates
+from Evaluation import chooseLinks
 
 import sys
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-wikipediaPageList = Util.wikipediaPageList
+############################################
 
+wikipediaPageList = Util.wikipediaPageList
 core = WikiCore(wikipediaPageList)
 
-for page_index in range(2):
-	print wikipediaPageList[page_index].title
+for page_index in range(3):
+	print "\n",page_index, wikipediaPageList[page_index].title
 	cand = core.generateCandidates(wikipediaPageList[page_index])
-	print "--"
-	print type(cand)
+
+	# pra poder trabalhar com indices
+	cand = list(cand)
+	
+	# agrupar em candidatos concorrentes de acordo com o indice de 'cand'
+	group = groupCandidates(cand)
+	score = []
+
 	for (f,w,i,j,ind) in cand:
 		#f  similaridade entre texto e titulo do link , w o termo sem stopword, i posição no array das palavras , j quantas palavras a partir do inicio, ind indice do link
-		words = wikipediaPageList[page_index].content.split(' ')
 		cos, dist = evalCandidate(wikipediaPageList, page_index, f, w, i, j, ind)
-		print str(f)+" || "+w+" || "+wikipediaPageList[ind].title+" | "+str(i)+" "+str(j)
-		print w, " | ", words[i], " | ", j, " | ", words[i:i+j], " | ", cos, dist
-		print "------------------------------------------------------------"
+		score.append((cos,dist,f))
 
+		# print str(f)+" || "+w+" || "+wikipediaPageList[ind].title+" | ", i, j, " | ", cos, dist
+		# words = wikipediaPageList[page_index].content.split(' ')
+		# print " - ", words[i:i+j]
+		# print "------------------------------------------------------------"
 
-# Teste Graph Distance Manuela
-# wikipediaPage = wikipediaPageList[0]
-# dictionary = Util.dictionary
-#
-# vizinho = dictionary.get(u"https://en.wikipedia.org/wiki/Discrete_mathematics")
-#
-# dist=-1
-# if vizinho is not None:
-#     dist = Util.getMinimumDistance(wikipediaPage, vizinho)
-# print "Distancia : ",dist
+	links = chooseLinks(cand, group, score)
+
+	# printing the links
+	print "LINKS GERADOS DA PAGINA"
+	for (w,idx,size,cos,dist,pageidx) in links:
+		link = wikipediaPageList[pageidx]
+		print "\t", w, idx, " -> ", link.title
